efefff3ac73a6446dad9afe07c0c93f7d674bd60
mephistosz <felipe.fg1@outlook.com>
2020-04-10 16:53:21 -0300

    BoardException and defensive programming

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import boardgame.BoardException;
 import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		try {
+			ChessMatch chessMatch = new ChessMatch();
+			UI.printBoard(chessMatch.getPieces());
+		} catch (BoardException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
